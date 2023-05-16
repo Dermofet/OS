7cd7bc79fb1b2a56a34333d25b6d55f19737c7e3
Инвар Пойда <pi-777@inbox.ru>
2023-05-16 23:17:57 +0300

    Update server and client

--- client.py
+++ client.py
@@ -1,58 +1,188 @@
 import socket
-import time
+import threading
 
 
 class Client:
-    def __init__(self, IP: str, PORT: int):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((IP, PORT))
+    def __init__(self, server_address: tuple):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect(server_address)
+        self.closed = False
+        self.receive_thread = threading.Thread(target=self._receive_messages)
+        self.receive_thread.start()
+        self.response_event = threading.Event()
 
-    @staticmethod
-    def _recvall(sock):
-        BUFF_SIZE = 4096
-        data = b''
-        while True:
-            part = sock.recv(BUFF_SIZE)
-            data += part
-            if not part:
+    def _receive_messages(self):
+        while not self.closed:
+            response = self.client_socket.recv(1)
+
+            if not response:
                 break
-        return data
 
-    def sendmsg(self, msg):
-        msg = str(msg).encode()
-        self.sock.sendall(msg)
-        return self.getmsg()
+            response = response.decode()
 
-    def getmsg(self):
-        response = self._recvall(self.sock).decode()
-        return response
+            if response == '0':
+                while True:
+                    response = self.client_socket.recv(4096).decode()
+                    print(response)
+                    if len(response) < 4096:
+                        break
+            elif response == '1':
+                response = self.client_socket.recv(4096)
+                if not response:
+                    break
+                response = response.split(b'\n')
+                print(response[0].decode())
 
-    def close(self):
-        time.sleep(1)  # Добавляем задержку перед закрытием сокета
-        self.sock.close()
+                file_size = int.from_bytes(response[1], "big")
+                file_data = b''.join(response[2:])
+                received_bytes = len(file_data)
+                while received_bytes < file_size:
+                    data = self.client_socket.recv(4096)
+                    if not data:
+                        break
+                    file_data += data
+                    received_bytes += len(data)
+                if received_bytes == file_size:
+                    with open("Bob/file.txt", "wb") as file:
+                        file.write(file_data)
+                    print("Файл успешно получен")
+                else:
+                    print(received_bytes)
+                    print("Ошибка при получении файла")
+
+            self.response_event.set()
+
+    def _sendmsg(self, message):
+        self.response_event.clear()  # Сбрасываем сигнал о получении ответа
+        self.client_socket.sendall(str(message).encode())
+
+    def _wait_for_response(self):
+        self.response_event.wait()  # Ожидаем получение ответа от сервера
 
     def register(self, username, password):
-        return self.sendmsg((0, 0, username, password))
+        self._sendmsg(0)
+        self._sendmsg((0, username, password))
+        self._wait_for_response()
 
     def login(self, username, password):
-        return self.sendmsg((1, 0, username, password))
+        self._sendmsg(1)
+        self._sendmsg((0, username, password))
+        self._wait_for_response()
 
-    def send_msg_to_all(self, username, msg):
-        return self.sendmsg((2, 2, username, msg))
+    def send_message_all_users(self, username, message):
+        self._sendmsg(2)
+        self._sendmsg((2, username, message))
+        self._wait_for_response()
 
-    def send_msg_to_user(self, sender_username, recipient_username, msg):
-        return self.sendmsg((3, 1, sender_username, recipient_username, msg))
+    def send_message(self, username, recipient, message):
+        self._sendmsg(3)
+        self._sendmsg((1, username, recipient, message))
+        self._wait_for_response()
 
-    # def send_file_to_user(self, ):
+    def send_file(self, username, recipient, file_path):
+        self._sendmsg(4)
+        self._sendmsg((1, username, recipient))
+        with open(file_path, "rb") as file:
+            file_data = file.read()
+        file_size = len(file_data).to_bytes(4, "big")
+        self.client_socket.sendall(file_size)
+        self.client_socket.sendall(file_data)
+        self.response_event.clear()
+        self.response_event.wait()
+
+    def close(self):
+        self.closed = True
+        self.client_socket.shutdown(socket.SHUT_RDWR)
+        self.receive_thread.join()
+        self.client_socket.close()
 
 
-def main():
-    client1 = Client('127.0.0.1', 8887)
-    # response = client1.sendmsg("aaaaaaaa")
-    response = client1.register("Иван", "qwerty")
-    print(response)
-    client1.close()
+class Menu:
+    @staticmethod
+    def menu(menu_text: str | list[str] | tuple[str]):
+        if isinstance(menu_text, str):
+            print(menu_text)
+        elif isinstance(menu_text, (list, tuple)):
+            for i, string in enumerate(menu_text):
+                print(f'{i}. {string}')
+
+    @staticmethod
+    def input() -> int:
+        # try:
+        choice = int(input("Ваш выбор: "))
+        return choice
+        # except E
+
+
+def run():
+    client = Client(("127.0.0.1", 8888))
+    menu_text = (
+            "Выход",
+            "Регистрация",
+            "Вход",
+            "Отправить сообщение всем пользователям",
+            "Отправить сообщение пользователю",
+            "Отправить файл"
+        )
+    while True:
+        Menu.menu(menu_text)
+        choice = Menu.input()
+
+        print()
+        if choice == -1:
+            print("Неверный ввод. Попробуйте еще раз.")
+            continue
+        elif choice == 0:
+            client.close()
+            print("Сессия закрыта")
+            break
+        elif choice == 1:
+            username = input("Ваше имя: ")
+            password = input("Пароль: ")
+            client.register(username, password)
+        elif choice == 2:
+            username = input("Ваше имя: ")
+            password = input("Пароль: ")
+            client.login(username, password)
+        elif choice == 3:
+            username = input("Ваше имя: ")
+            msg = input("Сообщение: ")
+            client.send_message_all_users(username, msg)
+        elif choice == 4:
+            username = input("Ваше имя: ")
+            recipient = input("Получатель: ")
+            msg = input("Сообщение: ")
+            client.send_message(username, recipient, msg)
+        elif choice == 5:
+            username = input("Ваше имя: ")
+            recipient = input("Получатель: ")
+            filepath = input("Путь к файлу: ")
+            client.send_file(username, recipient, filepath)
+        print()
+
+
+# def main():
+#     server_address = ("127.0.0.1", 8888)
+#     client1 = Client(server_address)
+#     client2 = Client(server_address)
+#     client3 = Client(server_address)
+#
+#     client1.register("Alice", "password")
+#     client2.register("Bob", "password")
+#     client3.register("Mary", "password")
+#
+#     client1.login("Alice", "password")
+#     client2.login("Bob", "password")
+#     client3.login("Mary", "password")
+#
+#     client1.send_message("Alice", "Bob", "Hello, Bob!")
+#     client2.send_message_all_users("Bob", "Hello everyone!")
+#     client1.send_file("Alice", "Bob", "Alice/testfile.txt")
+#
+#     client1.close()
+#     client2.close()
+#     client3.close()
 
 
 if __name__ == '__main__':
-    main()
+    run()
--- server.py
+++ server.py
@@ -1,156 +1,158 @@
 import socket
-from socketserver import ThreadingTCPServer, BaseRequestHandler
-
-from user import User, UserStatus
 import threading
-from threading import Thread
-
-import time
-from typing import Callable, Any, Self
+import ast
 
 
-class RequestHandler(BaseRequestHandler):
-    def handle(self):
-        print(f'New connection from {self.client_address}')
-        client_thread = Thread(target=self.server.process_message, args=(self.request, self.client_address))
-        client_thread.start()
-
-
-class Server(ThreadingTCPServer):
-    def __init__(self, server_address: tuple[str, int],
-                 RequestHandlerClass: Callable[[Any, Any, Self], BaseRequestHandler]):
-        super().__init__(server_address, RequestHandlerClass)
+class Server:
+    def __init__(self):
         self.users = {}
-
-    def process_message(self, client_socket, client_address):
-        def recvall(sock):
-            BUFF_SIZE = 4096
-            res = sock.recv(BUFF_SIZE)
-            return res
-
-        # request = tuple(eval(recvall(client_socket).decode()))
-        # print(f'Request: {request}')
-        message_type, address, username, *data = tuple(eval(recvall(client_socket).decode()))
-
-        match message_type:
-            case 0:  # (0, address, username, password)
-                msg = self.register(username, data[0])
-            case 1:  # (1, address, username, password), client_address
-                msg = self.login(username, data[0], client_address)
-            case 2:  # (2, address, sender, msg)
-                msg = self.send_msg_all_users(username, data[0])
-            case 3:  # (3, address, sender, recipient, msg)
-                msg = self.send_msg(username, *data)
-            case 4:  # (4, address, sender, recipient, file)
-                msg = self.send_file(username, *data)
-            case _:
-                msg = b"Unknown command"
-
-        print(f'Answer: {msg}')
-        client_socket.sendall(msg)
-        print("Client was disconnected\n")
-
-    def register(self, username, password):
-        if username in self.users:
-            return b"This user is already registered"
-
-        self.users[username] = User(username, password, UserStatus.INACTIVE, None)
-        return b"User successfully registered"
-
-    def login(self, username, password, address):
-        if username not in self.users:
-            return b"This user is not registered"
-
-        user = self.users[username]
-
-        if user.password != password:
-            return b"Wrong password"
-
-        if user.status == UserStatus.ACTIVE:
-            return b"This user is already logged in"
-
-        user.status = UserStatus.ACTIVE
-        user.address = address
-        return b"User successfully logged in"
+        self.active_users = {}
+        self.active_users_lock = threading.Lock()
+        self.users_lock = threading.Lock()
 
     @staticmethod
-    def _send_msg_to_active_user(sender, recipient, message, msg_type, timestamp):
-        recipient.incoming_messages.append({
-            "sender": recipient.username,
-            "msg": message,
-            "type": msg_type,
-            "timestamp": timestamp
-        })
+    def _recvall(request):
+        data = b''
+        while True:
+            part = request.recv(4096)
+            data += part
+            if len(part) < 4096:
+                break
+        return data
 
-        sender.outgoing_messages.append({
-            "recipient": sender.name,
-            "msg": message,
-            "type": msg_type,
-            "timestamp": timestamp
-        })
+    def handle_client(self, client_socket, client_address):
+        while True:
+            try:
+                message_type = client_socket.recv(1).decode()
 
-        if recipient.status == UserStatus.ACTIVE:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.connect(recipient.address)
-            sock.sendall(bytes(f'{recipient.username} - {msg}'))
-            sock.close()
+                if not message_type:
+                    client_socket.close()
+                    with self.active_users_lock:
+                        for username, info in self.active_users.items():
+                            if info["address"] == client_address:
+                                sock = self.active_users.pop(username)["socket"]
+                                sock.close()
+                                break
+                    print(f"Соединение с пользователем {client_address} закрыто")
+                    break
 
-    def send_msg_all_users(self, username, msg):
-        if username not in self.users:
-            return b"This user is not registered"
+                if message_type == '0':  # Регистрация на сервере
+                    _, username, password = ast.literal_eval(self._recvall(client_socket).decode())
+                    print(f'Регистрация пользователя {username}')
+                    if username not in self.users:
+                        with self.users_lock:
+                            self.users[username] = password
+                        response = "Регистрация прошла успешно"
+                    else:
+                        response = "Пользователь с таким именем уже зарегистрирован"
+                    response_type = 0
 
-        sender = self.users[username]
-        if sender.status == UserStatus.INACTIVE:
-            return b"This user is not logged in"
+                elif message_type == '1':  # Вход на сервер
+                    _, username, password = ast.literal_eval(self._recvall(client_socket).decode())
+                    print(f'Вход пользователя {username}')
+                    if username in self.users and self.users[username] == password:
+                        with self.active_users_lock:
+                            self.active_users[username] = {
+                                "socket": client_socket,
+                                "address": client_address
+                            }
+                        response = "Вход успешен"
+                    else:
+                        response = "Пользователь не зарегистрирован"
+                    response_type = 0
 
-        timestamp = time.time()
-        for name, user in self.users.items():
-            if name == username:
-                continue
+                elif message_type == '2':  # Отправка сообщения всем пользователям
+                    _, username, msg = ast.literal_eval(self._recvall(client_socket).decode())
+                    print(f'Отправка сообщения всем от {username}')
+                    if username in self.users:
+                        message_ = f"Сообщение от {username}: {msg}"
+                        for user in self.active_users.values():
+                            if user["address"] == client_address:
+                                continue
+                            with self.active_users_lock:
+                                user["socket"].sendall('0'.encode())
+                                user["socket"].sendall(message_.encode())
+                        response = "Сообщение отправлено всем пользователям"
+                    else:
+                        response = "Пользователь не зарегистрирован"
+                    response_type = 0
 
-            self._send_msg_to_active_user(sender, user, msg, timestamp, "msg")
+                elif message_type == '3':  # Отправка сообщения определенному пользователю
+                    _, username, recipient, msg = ast.literal_eval(self._recvall(client_socket).decode())
+                    print(f'Отправка сообщения от {username} к {recipient}')
+                    if username in self.users and recipient in self.users:
+                        if recipient in self.active_users:
+                            with self.active_users_lock:
+                                recipient_socket = self.active_users[recipient]["socket"]
+                                message_ = f"Сообщение от {username}: {msg}"
+                                recipient_socket.sendall('0'.encode())
+                                recipient_socket.sendall(message_.encode())
+                                response = f"Сообщение отправлено пользователю {recipient}"
+                        else:
+                            response = f"{recipient} не онлайн"
+                    else:
+                        response = "Пользователь не зарегистрирован"
+                    response_type = 0
 
-        return b"Sending successfully completed"
+                elif message_type == '4':  # Отправка файла определенному пользователю
+                    _, username, recipient = ast.literal_eval(self._recvall(client_socket).decode())
 
-    def send_msg(self, sender_username, recipient_username, msg):
-        if sender_username not in self.users:
-            return b"This user is not registered"
+                    file_size_bytes = client_socket.recv(4)
+                    if not file_size_bytes:
+                        break
+                    file_size = int.from_bytes(file_size_bytes, "big")
+                    received_bytes = 0
+                    file_data = b""
+                    while received_bytes < file_size:
+                        data = client_socket.recv(4096)
+                        if not data:
+                            break
+                        file_data += data
+                        received_bytes += len(data)
+                    if received_bytes == file_size:
+                        print(f'Отправка файла от {username} к {recipient}')
+                        if username in self.users and recipient in self.users:
+                            if recipient in self.active_users:
+                                with self.active_users_lock:
+                                    recipient_socket = self.active_users[recipient]["socket"]
+                                    message = f"Файл от {username}: размер {received_bytes} байт"
+                                    print(f'count_bytes = {received_bytes.to_bytes(4, "big")}')
+                                    recipient_socket.sendall('1'.encode() + message.encode() + b'\n' +
+                                                             received_bytes.to_bytes(4, "big") + b'\n' + file_data)
+                                    response = f"Файл отправлен пользователю {recipient}"
+                            else:
+                                response = f"{recipient} не онлайн"
+                        else:
+                            response = "Пользователь не зарегистрирован"
+                    else:
+                        print("Ошибка при получении файла")
+                    response_type = 0
 
-        sender = self.users[sender_username]
+                else:
+                    response = "Некорректный тип сообщения"
+                    response_type = 0
 
-        if recipient_username not in self.users:
-            return b"Recipient is not registered"
+                client_socket.sendall(str(response_type).encode())
+                client_socket.sendall(response.encode())
 
-        recipient = self.users[recipient_username]
+            except Exception as e:
+                print("Ошибка при обработке сообщения:", str(e))
+                print(client_socket, client_address)
+                break
 
-        if sender.status == UserStatus.INACTIVE:
-            return b"This user is not logged in"
+    def start_server(self, host, port):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind((host, port))
+        server_socket.listen(5)
+        print(f"Сервер запущен на {host}:{port}")
 
-        timestamp = time.time()
-        self._send_msg_to_active_user(sender, recipient, msg, timestamp, "msg")
-
-        return b"Sending successfully completed"
-
-    def send_file(self, sender_username, recipient_username, file):
-        if sender_username not in self.users:
-            return b"This user is not registered"
-
-        sender = self.users[sender_username]
-
-        if recipient_username not in self.users:
-            return b"Recipient is not registered"
-
-        recipient = self.users[recipient_username]
-
-        if sender.status == UserStatus.INACTIVE:
-            return b"This user is not logged in"
-
-        timestamp = time.time()
-        self._send_msg_to_active_user(sender, recipient, file, "file", timestamp)
-
-        return b"Sending successfully completed"
+        while True:
+            client_socket, client_address = server_socket.accept()
+            print("Установлено соединение с клиентом", client_address)
+            client_thread = threading.Thread(target=self.handle_client, args=(client_socket, client_address))
+            client_thread.start()
 
 
 if __name__ == '__main__':
-    with Server(('', 8887), RequestHandler) as server:
-        server.serve_forever()
+    server = Server()
+    server.start_server('127.0.0.1', 8888)
